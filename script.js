529b3112f2d8727a472e088502cc08b6dac6a746
JusteTitouan <titouan.germond@gmail.com>
2023-03-24 22:30:44 +0100

    GIGA Update
    Amélioration de l'UI
    Ajout de bord en fondu pour l'img scroller

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-var img_n = 0, max_img = document.querySelectorAll("#scroll>div>img").length
-var scroller = document.querySelector("#scroll>div")
+var img_n = 0, max_img = document.querySelectorAll("#scroll>#elmts>img").length
+var scroller = document.querySelector("#scroll>#elmts")
 
 function left() {
     img_n --
@@ -8,7 +8,7 @@ function left() {
         img_n = max_img - 1
     scroller.style.transform = "translate(-" + (img_n * (window.innerWidth > 700 ? 40 : 80)) + "vw, 0px)"
     clearTimeout(time_func)
-    time_func = setTimeout(left, 2000)
+    time_func = setTimeout(left, 4000)
 }
 function right() {
     img_n ++
@@ -16,7 +16,7 @@ function right() {
         img_n = 0
     scroller.style.transform = "translate(-" + (img_n * (window.innerWidth > 700 ? 40 : 80)) + "vw, 0px)"
     clearTimeout(time_func)
-    time_func = setTimeout(right, 2000)
+    time_func = setTimeout(right, 4000)
 }
 
-var time_func = setTimeout(right, 2000)
+var time_func = setTimeout(right, 4000)
